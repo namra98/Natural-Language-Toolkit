5cb024fb10a100b3a6599629bfe53279f11985f4
Namra Prajapati <nbrprajapati@gmail.com>
2018-08-01 20:13:45 +0530

    Initial commit

---
+++ testsent.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug  1 19:45:31 2018
+
+@author: NAMRA
+"""
+import sentiment_mod as s
+
+print(s.sentiment("This movie was awesome! The acting was great, plot was wonderful, and there were pythons...so yea!"))
+print(s.sentiment("This movie was utter junk. There were absolutely 0 pythons. I don't see what the point was at all. Horrible movie, 0/10"))
